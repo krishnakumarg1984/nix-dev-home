c2723d1994ce0fe3379cd1a43e6092ec19919143
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2024-03-12 16:31:48 +0530

    updated to list of packages

--- home/default.nix
+++ home/default.nix
@@ -25,11 +25,73 @@
   #
   # Search for packages here: https://search.nixos.org/packages
   home.packages = with pkgs; [
-    # Unix tools
-    ripgrep # Better `grep`
-    fd
-    # sd
-    tree
+    # azure-cli  # Next generation multi-platform command line experience for Azure
+    # bear  # Tool that generates a compilation database for clang tooling
+    # bibclean  # Prettyprint and syntax check BibTeX and Scribe bibliography data base files
+    # bitwise  # Terminal based bitwise calculator in curses
+    # sd  # Intuitive find & replace CLI (sed alternative)
+    act  # Run your GitHub Actions locally
+    aria  # A lightweight, multi-protocol, multi-source, command-line download utility
+    bash-completion  # Programmable completion for the bash shell
+    bat-extras.batdiff  # Diff a file against the current git index, or display the diff between two files. Requirements: bat, delta (optional)
+    bat-extras.batgrep  # Quickly search through and highlight files using ripgrep. Requirements: ripgrep
+    bottom  # A cross-platform graphical process/system monitor with a customizable interface
+    btop  # A monitor of resources
+    chezmoi  # Manage your dotfiles across multiple machines, securely
+    colormake  # Simple wrapper around make to colorize the output
+    cppcheck  # A static analysis tool for C/C++ code
+    delta  # A syntax-highlighting pager for git
+    detox  # Utility designed to clean up filenames
+    difftastic  # A syntax-aware diff
+    diskonaut  # Terminal disk space navigator
+    doxygen  # Source code documentation generator tool
+    dua   # A tool to conveniently learn about the disk usage of directories
+    duf  # Disk Usage/Free Utility
+    edtree  # File-tree visualizer and disk usage analyzer
+    eza  # A modern, maintained replacement for ls
+    f2  # Command-line batch renaming tool
+    fd  # A simple, fast and user-friendly alternative to find
+    fend  # Arbitrary-precision unit-aware calculator
+    file  # A program that shows the type of files
+    fzf  # A command-line fuzzy finder written in Go
+    gcc13  # GNU Compiler Collection, version 13.x.x (wrapper script)
+    gdu  # Disk usage analyzer with console interface
+    glow  # Render markdown on the CLI, with pizzazz!
+    gtrash  # A Trash CLI manager written in Go
+    hyperfine  # Command-line benchmarking tool
+    jless  # A command-line pager for JSON data
+    lazygit  # Simple terminal UI for git commands
+    lbzip2  # Parallel bzip2 compression utility
+    less # On ubuntu, we need this less for `man home-configuration.nix`'s pager to work.
+    lesspipe  # A preprocessor for less
+    llvmPackages_17.clangUseLLVM  # A C language family frontend for LLVM (wrapper script)
+    lua  # Powerful, fast, lightweight, embeddable scripting language
+    markdownlint-cli2  # A fast, flexible, configuration-based command-line interface for linting Markdown/CommonMark files with the markdownlint library
+    ninja  # Small build system with a focus on speed
+    nodejs_21  # Event-driven I/O framework for the V8 JavaScript engine
+    numbat  # High precision scientific calculator with full support for physical units
+    opam  # A package manager for OCaml
+    pdftk  # Command-line tool for working with PDFs
+    perl538Packages.Appcpanminus  # Get, unpack, build and install modules from CPAN
+    pigz  # A parallel implementation of gzip for multi-core machines
+    pixz  # A parallel compressor/decompressor for xz format
+    plzip  # A massively parallel lossless data compressor based on the lzlib compression library
+    prettyping  # A wrapper around the standard ping tool with the objective of making the output prettier, more colorful, more compact, and easier to read
+    procs  # A modern replacement for ps written in Rust
+    programmer-calculator  # A terminal calculator for programmers
+    ripgrep  # A utility that combines the usability of The Silver Searcher with the raw speed of grep
+    rsync  # Fast incremental file transfer utility
+    starship  # A minimal, blazing fast, and extremely customizable prompt for any shell
+    tealdeer  # A very fast implementation of tldr in Rust
+    tokei  # A program that allows you to count your code, quickly
+    topgrade  # Upgrade all the things
+    trash-cli  # Command line interface to the freedesktop.org trashcan
+    tree  # Command to produce a depth indented directory listing
+    viddy  # A modern watch command
+    wget  # Tool for retrieving files using HTTP, HTTPS, and FTP
+    yank  # Yank terminal output to clipboard
+    yazi  # Blazing fast terminal file manager written in Rust, based on async I/O
+    zellij  # A terminal workspace with batteries included
 
     # Nix dev
     cachix
@@ -38,13 +100,6 @@
     nixpkgs-fmt
     nixci
     nix-health
-
-    # Dev
-    # tmate
-
-    # On ubuntu, we need this less for `man home-configuration.nix`'s pager to
-    # work.
-    less
   ];
 
   home.shellAliases = {
